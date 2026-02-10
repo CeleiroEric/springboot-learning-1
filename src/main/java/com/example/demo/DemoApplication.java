5cbfb4f15ef0849c830af50330c63e2da21bfc0f
Eric Celeiro <celeiroericwork@gmail.com>
2026-02-10 03:30:07 +0100

    Add UsuarioService with listarUsuarios and guardarUsuario methods

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.model.Usuario;
 import com.example.demo.repository.UsuarioRepository;
+import com.example.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,15 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication implements CommandLineRunner {
 
 	@Autowired
-	private UsuarioRepository usuarioRepository;
+	private UsuarioService usuarioService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@Override
 	public void run(String... args) throws Exception{
-		usuarioRepository.save(new Usuario("Eric","celeiroericwork@gmail.com"));
-		System.out.println(usuarioRepository.findAll());
+		usuarioService.guardarUsuario(new Usuario("Eric","celeiroericwork@gmail.com"));
+		System.out.println(usuarioService.listarUsuarios());
 	}
 
 }
---
+++ src/main/java/com/example/demo/service/UsuarioService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarUsuarios(){
+        return usuarioRepository.findAll();
+    }
+    public Usuario guardarUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+}
